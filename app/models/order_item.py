5d579bd8282997b225fcc9f00374f998e499b6c2
ColeArt <92150341+Cole4rt@users.noreply.github.com>
2025-06-24 20:25:44 +0200

    Add method to the model Class: dish, order_item, reservation

--- app/models/order_item.py
+++ app/models/order_item.py
@@ -8,3 +8,55 @@ class OrderItem(db.Model):
     quantity = db.Column(db.Integer, default=1)
     is_takeaway = db.Column(db.Boolean, nullable=False)
     applied_price = db.Column(db.Numeric(10,2), nullable=False)
+
+    def to_dict(self):
+        return {
+            'item_id': self.item_id,
+            'reservation_id': self.reservation_id,
+            'dish_id': self.dish_id,
+            'quantity': self.quantity,
+            'is_takeaway': self.is_takeaway,
+            'applied_price': float(self.applied_price),
+        }
+
+    # -------------------
+    # Create from dict
+    # -------------------
+    @classmethod
+    def create_from_dict(cls, data):
+        return cls(
+            reservation_id=data['reservation_id'],
+            dish_id=data['dish_id'],
+            quantity=data.get('quantity', 1),
+            is_takeaway=data['is_takeaway'],
+            applied_price=data['applied_price']
+        )
+
+    # -------------------
+    # Read by ID
+    # -------------------
+    @classmethod
+    def get_by_id(cls, item_id):
+        return cls.query.get(item_id)
+
+    # -------------------
+    # Read all items for a reservation
+    # -------------------
+    @classmethod
+    def get_by_reservation(cls, reservation_id):
+        return cls.query.filter_by(reservation_id=reservation_id).all()
+
+    # -------------------
+    # Update from dict
+    # -------------------
+    def update_from_dict(self, data):
+        for field in ['reservation_id', 'dish_id', 'quantity', 'is_takeaway', 'applied_price']:
+            if field in data:
+                setattr(self, field, data[field])
+
+    # -------------------
+    # Delete method
+    # -------------------
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
